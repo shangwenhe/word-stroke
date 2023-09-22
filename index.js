ddca602f6199d3b2d967e594817088d8070a7e51
shangwenhe <shangwenhe@rongcloud.cn>
2023-09-22 14:40:46 +0800

    FEAT: 按笔顺拆分字

---
+++ index.js
@@ -0,0 +1,55 @@
+const Koa = require('koa');
+const {koaBody} = require('koa-body');
+const session = require('koa-session');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const router = require('@koa/router')();
+
+
+
+const app = module.exports = new Koa();
+
+app.use(session(app));
+// app.use(koaBody());
+
+
+/**
+ * route
+ */
+router.get('/get/:word', word);
+router.get('/', (ctx)=>{
+  return new Promise((resolve)=>{
+    ctx.set('Content-Type', 'text/html');
+    fs.readFile('./index.html', 'utf8', (error, data) => {
+      console.log('error', data);
+      ctx.set('Content-Type', 'text/html')
+      ctx.body = data;
+      resolve();
+    })
+  })
+});
+
+app.use(router.routes());
+function word(ctx) {
+  const getWords = ctx.params.word.split('').map((key)=>{
+    return axios.get(`https://hanyu.baidu.com/s?wd=${encodeURIComponent(key)}&ptype=zici`).then(res=>{
+      const $ = cheerio.load(res.data);
+      const pinyin = $('#pinyin').html()?.trim();
+      const wordStroke = $('.word-stroke .word-stroke-wrap').html()?.trim();
+      return {
+        name: key,
+        pinyin,
+        wordStroke
+      }
+    })
+  });
+  return Promise.all(getWords).then((data)=>{
+    ctx.body = {
+      word: ctx.params.word,
+      data
+    }
+  })
+}
+app.listen(3000);
